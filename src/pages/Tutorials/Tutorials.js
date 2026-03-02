d8869d295e1e072842c9a891fa1352663df6b5d5
Melo <102263606+Melo1337@users.noreply.github.com>
2026-03-02 16:17:41 -0300

    final tutorial

--- src/pages/Tutorials/Tutorials.js
+++ src/pages/Tutorials/Tutorials.js
@@ -5,39 +5,73 @@ import tutorial from './Tutorials.css'
 
 const Tutorials = () => {
 
+  function tutorial1212() {
+    return {
+      "Reset de cilindro": "x3R-t4_XNIk?si=IJY9w0_5Tukq0gko",
+    }
+  }
+
+  function tutorial1617() {
+    return {
+      "Reset do Toner":"xjr-k1qrDfg?si=uGfcfphporeRG8LD",
+      "Troca de suprimentos":"XLO32Hbwtlc?si=AIgETtpt6M637ZA7",
+    }
+  }
+
   function tutorial2540() {
     return {
-      "Imprimir configuração de rede": "https://www.youtube.com/embed/mVowccKhGMU?si=xPstGU8-uSBJHlaC",
-      "Contador de páginas": "https://www.youtube.com/embed/yy-Ffu0yDps?si=aUn4d93waCAlryie",
-      "Número de série": "https://www.youtube.com/embed/Z8YWI9urIyk?si=G8YNz2Si4I5qQbT6",
-      "Como conectar na rede wifi ": "https://www.youtube.com/embed/q4_XiqbiCiw?si=8ZLgwhcJQDTi0ya5",
-      "Reset de cilindro": "https://www.youtube.com/embed/NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
-      "Erro de cilindro": "https://www.youtube.com/embed/NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
+      "Imprimir configuração de rede": "mVowccKhGMU?si=xPstGU8-uSBJHlaC",
+      "Contador de páginas": "yy-Ffu0yDps?si=aUn4d93waCAlryie",
+      "Número de série": "Z8YWI9urIyk?si=G8YNz2Si4I5qQbT6",
+      "Como conectar na rede wifi ": "q4_XiqbiCiw?si=8ZLgwhcJQDTi0ya5",
+      "Reset de cilindro": "NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
+      "Erro de cilindro": "NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
+      "Trocar de suprimentos (Toner e Cilindro)":"OUZPBRv8gVw?si=YZv5Q_hwWmgD-bN7"
+    }
+  }
+
+  function tutorial5652() {
+    return {
+      "Resetar o cilindro":"6GkvMVpgjDg?si=p0Erohf90HeqBUJK",
+      "Erro de cilindro":"6GkvMVpgjDg?si=p0Erohf90HeqBUJK",
+      "Resetar o Toner":"Zwleny5Fh2k?si=C6lG--L7U643Z2r3",
     }
   }
 
   function tutorial7065() {
     return {
-      "Troca de cartucho de toner": "https://www.youtube.com/embed/wf884_XWfqo?si=OBELXfxci9ktJTiI"
+      "Troca de cartucho de toner": "wf884_XWfqo?si=OBELXfxci9ktJTiI",
+      "Reset de cilindro": "7LU_7OcF_IQ?si=XR2W8fH927fV1zM0"
     }
   }
 
   function tutorial8157() {
     return {
-      "Erro de cilindro": "https://www.youtube.com/embed/fYDQFff76zw?si=U9x10DxX6XV6_LBh",
-      "Reset de cilindro": "https://www.youtube.com/embed/fYDQFff76zw?si=U9x10DxX6XV6_LBh",
+      "Erro de cilindro": "fYDQFff76zw?si=U9x10DxX6XV6_LBh",
+      "Reset de cilindro": "fYDQFff76zw?si=U9x10DxX6XV6_LBh",
     }
   }
 
   const dadosImpressoras = {
     Brother: {
-      "DCP-L2540DW": tutorial2540(),
-      "DCP-L2540DN": tutorial2540(),
-      "DCP-L2520DW": tutorial2540(),
+      "DCP-1202": tutorial1212(),
+      "DCP-1212": tutorial1212(),
+      "DCP-1222": tutorial1212(),
+
+      "DCP-1617": tutorial1617(),
+      
+      "DCP-L2540": tutorial2540(),
+      "DCP-L2520": tutorial2540(),
+      "MFC-L2700": tutorial2540(),
+      
+      "DCP-L5652": tutorial5652(),
+      "DCP-L5602": tutorial5652(),
+      
+      "DCP-7065": tutorial7065(),
+      
       "DCP-8157": tutorial8157(),
       "DCP-8112": tutorial8157(),
       "DCP-8912": tutorial8157(),
-      "DCP-7065": tutorial7065(),
     },
   };
 
@@ -49,7 +83,7 @@ const Tutorials = () => {
 
     if (modeloSelecionado && marcaSelecionada && tutorialSelecionado) {
       const linkVideo = dadosImpressoras[marcaSelecionada][modeloSelecionado][tutorialSelecionado];
-      return <iframe width="560" height="315" src={linkVideo} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      return <iframe width="560" height="315" src={`https://www.youtube.com/embed/${linkVideo}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     } else {
       return <iframe width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     }
@@ -59,7 +93,7 @@ const Tutorials = () => {
     <>
       <Header />
       <section className='container-tutorial'>
-        <h2 className='title'>Selecione abaixo o modelo do seu equipamento e qual tutorial:</h2>
+        <h2 className='title'>Selecione abaixo o modelo do seu equipamento e o tutorial desejado:</h2>
         <form>
 
           <div className='seletores'>
