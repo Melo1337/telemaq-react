089b440f2757eddaf56a7501d91859115403d367
Melo <102263606+Melo1337@users.noreply.github.com>
2026-03-02 19:18:43 -0300

    .

---
+++ src/data/json/impTutorialList.js
@@ -0,0 +1,40 @@
+const tutorial1212 = () => ({
+     "Reset de cilindro": "x3R-t4_XNIk?si=IJY9w0_5Tukq0gko" 
+    });
+const tutorial1617 = () => ({
+     "Reset do Toner": "xjr-k1qrDfg?si=uGfcfphporeRG8LD",
+     "Troca de suprimentos": "XLO32Hbwtlc?si=AIgETtpt6M637ZA7"
+    });
+const tutorial2540 = () => ({
+    "Imprimir configuração de rede": "mVowccKhGMU?si=xPstGU8-uSBJHlaC",
+    "Contador de páginas": "yy-Ffu0yDps?si=aUn4d93waCAlryie",
+    "Número de série": "Z8YWI9urIyk?si=G8YNz2Si4I5qQbT6",
+    "Como conectar na rede wifi ": "q4_XiqbiCiw?si=8ZLgwhcJQDTi0ya5",
+    "Reset de cilindro": "NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
+    "Erro de cilindro": "NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
+    "Trocar de suprimentos (Toner e Cilindro)": "OUZPBRv8gVw?si=YZv5Q_hwWmgD-bN7" });
+const tutorial5652 = () => ({
+    "Resetar o cilindro": "6GkvMVpgjDg?si=p0Erohf90HeqBUJK",
+    "Erro de cilindro": "6GkvMVpgjDg?si=p0Erohf90HeqBUJK",
+    "Resetar o Toner": "Zwleny5Fh2k?si=C6lG--L7U643Z2r3" });
+const tutorial7065 = () => ({
+    "Troca de cartucho de toner": "wf884_XWfqo?si=OBELXfxci9ktJTiI",
+    "Reset de cilindro": "7LU_7OcF_IQ?si=XR2W8fH927fV1zM0" });
+const tutorial8157 = () => ({
+    "Erro de cilindro": "fYDQFff76zw?si=U9x10DxX6XV6_LBh",
+    "Reset de cilindro": "fYDQFff76zw?si=U9x10DxX6XV6_LBh" });
+
+const mapearImps = (lista, tutorialFn) => 
+    Object.fromEntries(lista.map(nome => [nome, tutorialFn()]));
+
+export const dadosImpressoras = {
+    Brother: {
+        ...mapearImps(["DCP-1212", "DCP-1222", "DCP-1202"], tutorial1212),
+        ...mapearImps(["DCP-L2540", "DCP-L2520", "MFC-L2700"], tutorial2540),
+        ...mapearImps(["DCP-L5652", "DCP-L5602"], tutorial5652),
+        ...mapearImps(["DCP-8157", "DCP-8112", "DCP-8912"], tutorial8157),
+        
+        "DCP-1617": tutorial1617(),
+        "DCP-7065": tutorial7065(),
+    },
+};
--- src/pages/Tutoriais/Tutoriais.js
+++ src/pages/Tutoriais/Tutoriais.js
@@ -1,79 +1,10 @@
 import React, { useState } from 'react';
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import { dadosImpressoras } from '../../data/json/impTutorialList'
 import './Tutoriais.css'
 
 const Tutoriais = () => {
-
-  function tutorial1212() {
-    return {
-      "Reset de cilindro": "x3R-t4_XNIk?si=IJY9w0_5Tukq0gko",
-    }
-  }
-
-  function tutorial1617() {
-    return {
-      "Reset do Toner":"xjr-k1qrDfg?si=uGfcfphporeRG8LD",
-      "Troca de suprimentos":"XLO32Hbwtlc?si=AIgETtpt6M637ZA7",
-    }
-  }
-
-  function tutorial2540() {
-    return {
-      "Imprimir configuração de rede": "mVowccKhGMU?si=xPstGU8-uSBJHlaC",
-      "Contador de páginas": "yy-Ffu0yDps?si=aUn4d93waCAlryie",
-      "Número de série": "Z8YWI9urIyk?si=G8YNz2Si4I5qQbT6",
-      "Como conectar na rede wifi ": "q4_XiqbiCiw?si=8ZLgwhcJQDTi0ya5",
-      "Reset de cilindro": "NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
-      "Erro de cilindro": "NlppUmYf9wA?si=LOv0hRW4yhNKcuj8",
-      "Trocar de suprimentos (Toner e Cilindro)":"OUZPBRv8gVw?si=YZv5Q_hwWmgD-bN7"
-    }
-  }
-
-  function tutorial5652() {
-    return {
-      "Resetar o cilindro":"6GkvMVpgjDg?si=p0Erohf90HeqBUJK",
-      "Erro de cilindro":"6GkvMVpgjDg?si=p0Erohf90HeqBUJK",
-      "Resetar o Toner":"Zwleny5Fh2k?si=C6lG--L7U643Z2r3",
-    }
-  }
-
-  function tutorial7065() {
-    return {
-      "Troca de cartucho de toner": "wf884_XWfqo?si=OBELXfxci9ktJTiI",
-      "Reset de cilindro": "7LU_7OcF_IQ?si=XR2W8fH927fV1zM0"
-    }
-  }
-
-  function tutorial8157() {
-    return {
-      "Erro de cilindro": "fYDQFff76zw?si=U9x10DxX6XV6_LBh",
-      "Reset de cilindro": "fYDQFff76zw?si=U9x10DxX6XV6_LBh",
-    }
-  }
-
-  const dadosImpressoras = {
-    Brother: {
-      "DCP-1202": tutorial1212(),
-      "DCP-1212": tutorial1212(),
-      "DCP-1222": tutorial1212(),
-
-      "DCP-1617": tutorial1617(),
-      
-      "DCP-L2540": tutorial2540(),
-      "DCP-L2520": tutorial2540(),
-      "MFC-L2700": tutorial2540(),
-      
-      "DCP-L5652": tutorial5652(),
-      "DCP-L5602": tutorial5652(),
-      
-      "DCP-7065": tutorial7065(),
-      
-      "DCP-8157": tutorial8157(),
-      "DCP-8112": tutorial8157(),
-      "DCP-8912": tutorial8157(),
-    },
-  };
 
   const [marcaSelecionada, setMarcaSelecionada] = useState("");
   const [modeloSelecionado, setModeloSelecionado] = useState("");
@@ -93,7 +24,10 @@ const Tutoriais = () => {
     <>
       <Header />
       <section className='container-tutorial'>
-        <h2 className='title'>Selecione abaixo o modelo do seu equipamento e o tutorial desejado:</h2>
+        <div>
+          <h2 className='title'>Selecione abaixo o modelo do seu equipamento e o tutorial desejado:</h2>
+          <p>OBS: O video será gerado quando todos campos forem selecionados!</p>
+        </div>
         <form>
 
           <div className='seletores'>
@@ -127,7 +61,7 @@ const Tutoriais = () => {
           </div>
         </form>
         
-        <div className='container-video'>
+        <div className='container-video'> 
           <div className='video'>
             {gerarVideo()}
           </div>
