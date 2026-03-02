4c9dc29863bf8e60fba66899be912ede44f8d04e
Melo <102263606+Melo1337@users.noreply.github.com>
2026-03-02 16:30:37 -0300

    update netlify

--- src/pages/Tutorials/Tutorials.js
+++ src/pages/Tutorials/Tutorials.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import tutorial from './Tutorials.css'
+import './Tutorials.css'
 
 const Tutorials = () => {
 
