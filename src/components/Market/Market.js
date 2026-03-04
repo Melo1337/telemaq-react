275286f2303cbd0193d7abe6dfe5045f175f379e
Melo <102263606+Melo1337@users.noreply.github.com>
2026-03-04 18:22:54 -0300

    test2

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -9,7 +9,23 @@ function Market() {
             {Object.entries(listPrinters).map(([nomeDaCategoria, listaDeProdutos]) => {
                 return (
                     <div key={nomeDaCategoria} className={nomeDaCategoria}>
-                        <p>{nomeDaCategoria}</p>
+                        <h1>{nomeDaCategoria}</h1>
+
+                        {listaDeProdutos.map((product, index) => (
+                            <div className="printer" id={index}>
+                                <h3>{product.nome}</h3>
+                                <img src={`/img/${product.imagemSrc}.jpg`} alt={product.nome} />
+                                <p>{product.descricao}</p>
+                                <p><strong>R$ {product.preco},00</strong></p>
+
+                                <div className="buttons">
+                                    <ButtomWhatsapp texto={"Comprar"} link={`Olá! Gostaria de saber mais sobre a impressora ${product.nome} que está anunciada no site!`} />
+                                    <Link to={`/PrinterDetails/${product.nome}`} state={{ id: index }}>
+                                        <ButtomWhatsapp texto={"Ver detalhes"} />
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 );
             })}
