5eec537b6e3e44fbf0002c7e85a4054478d2c265
Melo <102263606+Melo1337@users.noreply.github.com>
2026-03-07 12:31:38 -0300

    .

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 import './Market.css'
 
 function Market() {
+
     return (
         <>
             {Object.entries(listPrinters).map(([nomeDaCategoria, listaDeProdutos]) => (
                 <div key={nomeDaCategoria} className={nomeDaCategoria}>
                     <h1>Impressoras a venda</h1>
 
-                    <div className="printers">
+                    <div className="products">
                         {listaDeProdutos.map((product, index) => (
                             <div className="printer" id={index}>
                                 <h3>{product.nome}</h3>
@@ -28,14 +29,26 @@ function Market() {
                             </div>
                         ))}
                     </div>
-                    <div className="products">
-                        {Object.entries(productsList).map(([nomeDaCategoria]) => (
-                            <div className={nomeDaCategoria}>
-                                <h1>{nomeDaCategoria}</h1>
-                                
-                            </div>
-                        ))}
-                    </div>
+
+                    {Object.entries(productsList).map(([nomeDaCategoria]) => (
+                        <><h1>{nomeDaCategoria}</h1>
+                            <div className={`products ${nomeDaCategoria}`}>
+
+                                {productsList[nomeDaCategoria].map((product, index) => (
+                                    <div className="printer" id={product.codigo}>
+                                    <h3>{product.descricao}</h3>
+                                    <img src={`/img/products/${product.codigo}.jpeg`} alt={product.nome} />
+                                    <p>Quantidade disponivel: {product.estoque}</p>
+                                    <p><strong>R$ {product.preco_venda},00</strong></p>
+                                    <p>codigo: {product.codigo}</p>
+
+                                    <div className="buttons">
+                                        <ButtomWhatsapp texto={"Comprar"} link={`Olá! Gostaria de saber mais sobre a impressora ${product.nome} que está anunciada no site!`} />
+                                    </div>
+                                </div>
+                                ))}
+                            </div></>
+                    ))}
                 </div>
             ))}
         </>
