82f9b1a97b812dc026055a1dc5c7b0b57cf4827c
Melo <102263606+Melo1337@users.noreply.github.com>
2026-03-31 09:41:12 -0300

    update market api products photos

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,17 +1,36 @@
 import listPrinters from "../../data/json/products.json";
-import productsList from "../../data/json/filter.json";
+import products from "../../data/json/filter.json";
 import ButtomWhatsapp from "../ButtomWhatsapp/ButtomWhatsapp";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import './Market.css'
 
+
 function Market() {
+
+  const [dados, setDados] = useState({});
+
+  useEffect(() => {
+    fetch('http://192.168.2.254:3001/api/filter', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setDados(data);
+      })
+      .catch((err) => 
+        console.log(`Api indisponivel, carregando arquivo local... ${err}`),
+        setDados(products)
+    );
+  }, []);
 
     return (
         <>
             <section id="produtos">
                 <div className="container">
 
-                    {Object.entries(listPrinters).map(([nomeDaCategoria, listaDeProdutos]) => (
+{Object.entries(listPrinters).map(([nomeDaCategoria, listaDeProdutos]) => (
                         <div key={nomeDaCategoria} className={nomeDaCategoria}>
                             <h1>Impressoras</h1>
 
@@ -19,7 +38,7 @@ function Market() {
                                 {listaDeProdutos.map((product, index) => (
                                     <div className="product" id={index}>
                                         <h3>{product.nome}</h3>
-                                        <div><img src={`/img/${product.imagemSrc}.jpg`} alt={product.nome} /></div>
+                                        <div className="img"><img src={`/img/${product.imagemSrc}.jpg`} alt={product.nome} /></div>
                                         <p>{product.descricao}</p>
                                         <p><strong>R$ {product.preco},00</strong></p>
 
@@ -35,29 +54,32 @@ function Market() {
                         </div>
                     ))}
 
-                    {Object.entries(productsList).map(([nomeDaCategoria]) => (
+                    {dados && Object.entries(dados).map(([nomeDaCategoria]) => (
                         <div key={nomeDaCategoria} className={nomeDaCategoria}>
                             <h1>{nomeDaCategoria}</h1>
                             <div className={`products ${nomeDaCategoria}`}>
 
-                                {productsList[nomeDaCategoria].map((product, index) => (
+                                {dados[nomeDaCategoria].map((product, index) => (
                                     <div className="product" id={product.codigo}>
-                                        <h3>{product.descricao}</h3>
+                                        <h3>{product.marca}</h3>
                                         <div className="img">
                                             <img src={`/img/products/${product.codigo}.webp`} alt={product.nome} />
                                         </div>
+                                        <p className="descricao"><strong>Compativel: </strong>{product.descricao}</p>
+                                        <p className="lines">___________________________________</p>
                                         <p>Quantidade disponivel: {product.estoque}</p>
                                         <p><strong>R$ {product.preco_venda},00</strong></p>
                                         <p>codigo: {product.codigo}</p>
 
                                         <div className="buttons">
-                                            <ButtomWhatsapp texto={"Comprar"} link={`Olá! Gostaria de saber mais sobre o ${product.nome} que está anunciada no site!`} />
+                                            <ButtomWhatsapp texto={"Comprar"} link={`Olá! Gostaria de saber mais sobre o ${product.marca} que está anunciada no site!`} />
                                         </div>
                                     </div>
                                 ))}
                             </div>
                         </div>
                     ))}
+
                 </div>
             </section>
         </>
